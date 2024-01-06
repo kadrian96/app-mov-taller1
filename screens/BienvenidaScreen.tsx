94ee7a9f3eb22b7b1828b12803e363a2f4c7c2b7
kadrian96 <w.kevinm@hotmail.com>
2024-01-05 22:13:13 -0500

    juego

--- screens/BienvenidaScreen.tsx
+++ screens/BienvenidaScreen.tsx
@@ -4,6 +4,7 @@ import React from 'react'
 import { TouchableOpacity } from 'react-native-gesture-handler'
 
 export default function BienvenidaScreen({navigation}: any) {
+  const insecto=['hormiga','abeja','araña','cucaracha','escarabajo']
   
   return (
     <ImageBackground
@@ -14,20 +15,20 @@ export default function BienvenidaScreen({navigation}: any) {
         <Text style={styles.titulo}>Bienvenido al juego</Text>
         <Text style={styles.subtitulo}>Escoje el insecto que quieres aplastar!!</Text>
 
-      <TouchableOpacity style={styles.btn} onPress={()=> navigation.navigate('Juego')}>
+      <TouchableOpacity style={styles.btn} onPress={()=> navigation.navigate('Juego',insecto[0])}>
             <Text style={styles.textbutton}>Hormiga 🐜</Text>
       </TouchableOpacity>
 
-      <TouchableOpacity style={styles.btn} onPress={()=> navigation.navigate('Juego')}>
+      <TouchableOpacity style={styles.btn} onPress={()=> navigation.navigate('Juego',insecto[1])}>
             <Text style={styles.textbutton}>Abeja 🐝</Text>
       </TouchableOpacity>
-      <TouchableOpacity style={styles.btn} onPress={()=> navigation.navigate('Juego')}>
+      <TouchableOpacity style={styles.btn} onPress={()=> navigation.navigate('Juego',insecto[2])}>
             <Text style={styles.textbutton}>Araña 🕷️</Text>
       </TouchableOpacity>
-      <TouchableOpacity style={styles.btn} onPress={()=> navigation.navigate('Juego')}>
+      <TouchableOpacity style={styles.btn} onPress={()=> navigation.navigate('Juego',insecto[3])}>
             <Text style={styles.textbutton}>Cucaracha 🪳</Text>
       </TouchableOpacity>
-      <TouchableOpacity style={styles.btn} onPress={()=> navigation.navigate('Juego')}>
+      <TouchableOpacity style={styles.btn} onPress={()=> navigation.navigate('Juego',insecto[4])}>
             <Text style={styles.textbutton}>Escarabajo 🪲</Text>
       </TouchableOpacity>
 
--- screens/JuegoScreen.tsx
+++ screens/JuegoScreen.tsx
@@ -1,12 +1,50 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { ImageBackground, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
-export default function JuegoScreen() {
+export default function JuegoScreen({route}:any) {
+    const insecto=route.params
+    console.log(insecto);
   return (
-    <View>
-      <Text>JuegoScreen</Text>
-    </View>
+    <ImageBackground 
+    source={require('../assets/image/lodo.jpg')}
+    style={styles.container}
+    >
+        <View  style={styles.textocontain}>
+            <Text style={styles.texto}>Score:</Text>
+            <Text style={styles.texto}>Tiempo:</Text>
+
+        </View>
+
+
+    </ImageBackground>
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        resizeMode:'cover',
+        
+    },
+    textocontain:{
+        width:'100%',
+        height:50,
+        backgroundColor:'#B2BEB5',
+        alignItems:'center',
+        flexDirection:'row',
+        alignContent:'center',
+        paddingHorizontal:40
+    },
+    texto:{
+        textAlign:'center',
+        marginHorizontal:20,
+        fontSize:20,
+        color:'#E52B50',
+        fontWeight:'bold'
+
+
+
+        
+    }
+
+})
