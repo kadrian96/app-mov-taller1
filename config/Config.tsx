b3765643c1e1484da104f6253375a6c36848333d
JONATHANDEMOND <133927721+JONATHANDEMOND@users.noreply.github.com>
2024-01-05 21:56:24 -0500

    Creacion de estack login screen y configuracion de firebase

---
+++ config/Config.tsx
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import {getDatabase} from 'firebase/database';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCmUoGozorlIuAA1esKLAhIFH9wDX7NRwI",
+  authDomain: "taller01-28c90.firebaseapp.com",
+  projectId: "taller01-28c90",
+  storageBucket: "taller01-28c90.appspot.com",
+  messagingSenderId: "663675947597",
+  appId: "1:663675947597:web:2d86ac5caae065cd586a04"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getDatabase(app)
